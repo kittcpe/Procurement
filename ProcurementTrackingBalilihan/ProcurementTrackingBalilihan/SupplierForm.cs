ff82f7f78645a4dacfecbdf9ae8384a9eefac167
Kitt Cymonne Japson <japson.kittcymonne@gmail.com>
2022-11-03 10:02:45 +0800

    supplier_form

--- ProcurementTrackingBalilihan/ProcurementTrackingBalilihan/SupplierForm.cs
+++ ProcurementTrackingBalilihan/ProcurementTrackingBalilihan/SupplierForm.cs
@@ -6,15 +6,46 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ProcurementTrackingBalilihan.Dal;
 
 namespace ProcurementTrackingBalilihan
 {
     public partial class SupplierForm : DevExpress.XtraEditors.XtraForm
     {
+        DataTable SupplierData = new DataTable();
         public SupplierForm()
         {
             InitializeComponent();
         }
+        #region WaitForm
+        bool loadingIsAlreadyShowing = false;
+        private void ShowLoading(string message)
+        {
+            try
+            {
+                this.Enabled = false;
+                if (!loadingIsAlreadyShowing)
+                {
+                    loadingIsAlreadyShowing = true;
+                    splashScreenManager1.ShowWaitForm();
+                }
+                splashScreenManager1.SetWaitFormDescription(message);
+            }
+            catch { }
+        }
+
+        private void HideLoading()
+        {
+            try
+            {
+                this.Enabled = true;
+                loadingIsAlreadyShowing = false;
+                splashScreenManager1.CloseWaitForm();
+            }
+            catch { }
+        }
+        #endregion
+
 
         private void btnSave_Click(object sender, EventArgs e)
         {
@@ -30,16 +61,10 @@ namespace ProcurementTrackingBalilihan
                 }
                 else
                 {
-                    if (btnSave.Text == "Save")
+                    //bw
+                    if (!bwSaveSupplier.IsBusy)
                     {
-
-                        //save
-                    }
-                    else
-                    {
-
-                        //add
-
+                        bwSaveSupplier.RunWorkerAsync();
                     }
                 }
             }
@@ -70,6 +95,8 @@ namespace ProcurementTrackingBalilihan
         private void btnEditSupplier_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
             gcSupplierInfo.Visible = true;
+            txtname.Text = gvSupplier.GetRowCellValue(gvSupplier.FocusedRowHandle, "name").ToString();
+            txtaddress.Text = gvSupplier.GetRowCellValue(gvSupplier.FocusedRowHandle, "address").ToString();
             btnSave.Text = "Save";
         }
 
@@ -77,7 +104,97 @@ namespace ProcurementTrackingBalilihan
         {
             //load datatable
 
+            if (!bwViewSupplierList.IsBusy)
+            {
+                ShowLoading("Loading data . . . ");
+                bwViewSupplierList.RunWorkerAsync();
+            }
 
         }
+
+        private void bwViewSupplierList_DoWork(object sender, DoWorkEventArgs e)
+        {
+            SupplierData = Supplier.GetSupplierList();
+            bwViewSupplierList.CancelAsync();
+        }
+
+        private void bwViewSupplierList_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            HideLoading();
+            dtSupplier.DataSource = SupplierData;
+            lblNoData.Visible = SupplierData.Rows.Count > 0 ? false : true;
+        }
+
+        private void SupplierForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        int supplierId=0;
+        private void gvSupplier_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            if (gvSupplier.SelectedRowsCount > 0)
+            {
+                supplierId = Convert.ToInt32(gvSupplier.GetRowCellValue(gvSupplier.FocusedRowHandle, "id"));
+                lblid.Text = supplierId.ToString();
+            }
+        }
+
+        private void bwSaveSupplier_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ShowLoading("Saving Supplier . . .");
+            SaveSupplier();
+        }
+
+        private void bwSaveSupplier_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            HideLoading();
+            if (Supplier.isSaveSuccessfully == true)
+            {
+                MessageBox.Show("Saved Successfully!");
+                ViewList();
+            }
+            else
+            {
+                MessageBox.Show(Supplier.SaveErrorMessage);
+            }
+            btnSave.Enabled = true;
+            clearfields();
+
+        }
+        private void ViewList() {
+            if (!bwViewSupplierList.IsBusy) {
+                ShowLoading("Refreshing Table . . .");
+                bwViewSupplierList.RunWorkerAsync();
+            }
+
+            }
+        private void clearfields() {
+            txtname.Text = string.Empty;
+            txtaddress.Text = string.Empty;
+        }
+
+        private void SaveSupplier() {
+
+            btnSave.Enabled = false;
+            if (btnSave.Text == "Save")
+            {
+                //save
+                Supplier.AddSaveSupplier(txtname.Text, txtaddress.Text, supplierId);
+                
+            }
+            else
+            {
+                //add
+                Supplier.AddSaveSupplier(txtname.Text, txtaddress.Text, 0);
+
+            }
+        }
+
+        private void btnRefresh_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            ViewList();
+        }
+
+
     }
 }
